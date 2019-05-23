1e4b6779b8332e43c37604fcc9819e94b0aaec5f
mjimenez169492 <33692744+mjimenez169492@users.noreply.github.com>
2019-05-22 22:14:02 -0400

    Update MovesFactory.java

--- Object_Factories/MovesFactory.java
+++ Object_Factories/MovesFactory.java
@@ -62,8 +62,6 @@ public class MovesFactory
         move.setAccuracyModifier(0);
         move.setOutputModifier(0);
         move.setCriticalModifier(0);
-        move.setCriticalModifier(0);
-        move.setStressEffect(0);
         move.setMoveSpeed(0);
         
         move.setMasteryValue(0);
@@ -131,8 +129,6 @@ public class MovesFactory
         move.setAccuracyModifier(0);
         move.setOutputModifier(0);
         move.setCriticalModifier(0);
-        move.setCriticalModifier(0);
-        move.setStressEffect(0);
         move.setMoveSpeed(0);
         
         move.setMasteryValue(0);
@@ -142,5 +138,72 @@ public class MovesFactory
         return move;
     }
     
+    // see "new 46" for moves object creation data
+    public Moves getStandardAttack()
+    {
+        Moves move = new Moves();
+        
+        move.setName("Attack");
+        move.setDescription("User damages the target.");
+        
+        move.setApproach("Stamina");
+        move.setStyle("Universal Stamina");
+        move.setTechnique("Universal Stamina");
+        
+        move.setGaugeUsed("None");
+        move.addGaugeTargeted("Current Health");
+        
+        move.setEnchantment("None");
+        move.setMoveAvailabilityStatus("None");
+        
+        move.usableFromMainMenu(true);
+        move.usableInBattle(true);
+        move.usableIfKnockedDown(false);
+        move.usableIfGrabbed(false);
+        move.usableIfAirborne(false);
+        
+        move.setTarget("Any");
+        move.setTimesMoveAffectsTarget(1);
+        
+        move.setMoveUsedText(" attacked!");
+        move.setMoveMissedText("But the attack missed!");
+        move.setMoveFailedText("But it failed!");
+        
+        move.parryable(false);
+        move.counterable(false);
+        move.guardable(false);
+        move.grabBlockable(false);
+        
+        move.addStatusMoveAdds(null);
+        move.addStatusNegatingMove("None");
+        move.addStatusesMoveRemoves("None");
+        
+        move.setUserWeaponDurabilityEffect(0);
+        move.setWeaponDurabilityEffect(0);
+        move.setBodyArmorDurabilityEffect(0);
+        move.setLegArmorDurabilityEffect(0);
+        move.setFootArmorDurabilityEffect(0);
+        move.setAccessoryOneDurabilityEffect(0);
+        move.setAccessoryTwoDurabilityEffect(0);
+        
+        move.setClassification("Damage");
+        
+        move.setCustomAccuracy(0);
+        move.setCustomOutput(0);
+        move.setCustomCritical(0);
+        
+        move.setMoveCost(0);
+        move.setOutputVariance(0.30);
+        move.setAccuracyModifier(3);
+        move.setOutputModifier(0.12);
+        move.setCriticalModifier(15);
+        move.setMoveSpeed(1.15);
+        
+        move.setMasteryValue(0);
+        move.setCurrentValue(0);
+        move.setMoveMasteryState(false);
+        
+        return move;
+    }
     
 }
